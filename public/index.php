12a0c3e2468415edf1d477665cce78448efe3282
cylmat <cyrilmatte.pro@gmail.com>
2022-10-11 19:57:45 +0000

    install symfony 5.4

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
 
-# Change with your own application's index.php
-echo include __DIR__.'/_servers.php';
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
