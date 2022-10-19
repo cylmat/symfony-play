e17b5c138ffdca96c12a062a3b5c129045c76753
cylmat <cyrilmatte.pro@gmail.com>
2022-10-19 13:47:51 +0000

    fix assets

---
+++ public/assets/script/app.js
@@ -0,0 +1,7 @@
+const $ = require('jquery');
+require('bootstrap');
+// e.g. require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    // $('[data-toggle="popover"]').popover();
+});
