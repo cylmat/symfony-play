647443e03886a04c03a7532ac906f14480ba0178
cylmat <cyrilmatte.pro@gmail.com>
2022-10-24 08:32:57 +0000

    remove chunk

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,11 @@ Encore
     .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    // Avoid dependencies duplication.
-    .splitEntryChunks()
-    .configureSplitChunks(function(splitChunks) {
-        splitChunks.minSize = 0;
-    })
+    // Avoid dependencies duplication, and change "filename" as "filename.123hash123.js"
+    // .splitEntryChunks()
+    // .configureSplitChunks(function(splitChunks) {
+    //     //splitChunks.minSize = 0;
+    // })
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
