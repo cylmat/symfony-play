14dafc69022a9e0defada893a6ffe6c903d40d72
cylmat <cyrilmatte.pro@gmail.com>
2023-01-06 17:19:39 +0000

    action functional kerneltestcase

--- tests/Component/Encrypt/Application/Action/EncryptActionTest.php
+++ tests/Component/Encrypt/Application/Action/EncryptActionTest.php
@@ -3,7 +3,6 @@
 namespace App\Tests\Encrypt\Application\Action;
 
 use App\Encrypt\Application\Action\EncryptAction;
-use App\Encrypt\Domain\Manager\EncryptManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
@@ -13,21 +12,12 @@ final class EncryptActionTest extends KernelTestCase
 {
     protected function setUp(): void
     {
-        self::bootKernel();
-        $container = static::getContainer();
-
-        $this->encryptManager = $this->createMock(EncryptManager::class);
-        $this->encryptAction = new EncryptAction($this->encryptManager);
+        $this->encryptAction = static::getContainer()->get(EncryptAction::class);
     }
 
     public function testExecute(): void
     {   
-        $this->encryptManager
-            ->method('encryptValue')
-            ->with('bcrypt', 'value', ['option'])
-            ->willReturn('$2y$');
-
         $res = $this->encryptAction->execute('value', ['option']);
-        $this->assertSame('$2y$', $res);
+        $this->assertStringStartsWith('$2y$', $res);
     }
 }
