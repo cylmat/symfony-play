849eb1a9fb72b64490ceb241c59f283dbba3cb38
Cyril Matté <cyril_matte@ext.carrefour.com>
2023-05-17 14:29:05 +0000

    parameters local

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,32 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /** @SuppressWarnings(PHPMD) Unused code */
+    /* @phpstan-ignore-next-line: unused private method */
+    private function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
+    {
+        $configDir = $this->getConfigDir();
+
+        $container->import($configDir.'/{packages}/*.{php,yaml}');
+        $container->import($configDir.'/{packages}/'.$this->environment.'/*.{php,yaml}');
+
+        if (is_file($configDir.'/services.yaml')) {
+            $container->import($configDir.'/services.yaml');
+            $container->import($configDir.'/{services}_'.$this->environment.'.yaml');
+        } else {
+            $container->import($configDir.'/{services}.php');
+        }
+
+        $container->import($configDir.'/parameters.yaml');
+        $container->import($configDir.'/local/parameters.{php,yaml}');
+    }
 }
