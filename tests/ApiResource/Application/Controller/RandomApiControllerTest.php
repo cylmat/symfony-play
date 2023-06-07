d74fd4083bd2a7a6ffe44a6cc49559566e2a75b4
Cyril Matté <cyril_matte@ext.carrefour.com>
2023-06-07 14:45:02 +0000

    fix

--- tests/ApiResource/Application/Controller/RandomApiControllerTest.php
+++ tests/ApiResource/Application/Controller/RandomApiControllerTest.php
@@ -2,19 +2,11 @@
 
 namespace App\Test\ApiResource;
 
-use App\ApiResource\Application\Controller\RandomApiController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /** @group functional */
 final class RandomApiControllerTest extends WebTestCase
 {
-    private RandomApiController $randomApiController;
-
-    protected function setUp(): void
-    {
-        $this->randomApiController = new RandomApiController();
-    }
-
     public function testGetRandomIntAction(): void
     {
         $client = static::createClient();
