0a6160971161c0a337174c96244d263998138bd3
Cyril Matté <cyril_matte@ext.carrefour.com>
2023-06-20 14:28:50 +0000

    session handler

---
+++ src/AppBundle/Application/Session/AppPdoSessionHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\AppBundle\Application\Session;
+
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
+
+class AppPdoSessionHandler extends PdoSessionHandler
+{
+}
