f9ef3fde6bfc044e59f57491231f942cc6459bbe
Cyril Matté <cyril_matte@ext.carrefour.com>
2023-06-20 15:53:58 +0000

    set redis local session

--- src/AppBundle/Application/Session/AppPdoSessionHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\AppBundle\Application\Session;
-
-use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
-
-class AppPdoSessionHandler extends PdoSessionHandler
-{
-}
