072bcbe37e75ffee511edab3403e78aa08426f58
cylmat <cyrilmatte.pro@gmail.com>
2023-12-28 20:51:51 +0000

    lib vendor composer

--- public/index.php
+++ public/index.php
@@ -7,7 +7,9 @@ error_reporting(-1);
 
 $_SERVER['APP_RUNTIME_OPTIONS']['project_dir'] = dirname(__DIR__, 1);
 
+require_once dirname(__DIR__).'/lib/vendor/autoload.php';
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// @todo lib-dev composer
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
