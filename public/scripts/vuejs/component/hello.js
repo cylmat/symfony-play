2f962b69ff896daa84769d8722cc7a2f6841cc74
cylmat <cyrilmatte.pro@gmail.com>
2024-01-11 16:19:49 +0000

    update

--- public/scripts/vuejs/component/hello.js
+++ public/scripts/vuejs/component/hello.js
@@ -1,12 +1,15 @@
 export default {
-  data() {
-      return {
-          message: 'Hello Vue !'
-      }
-  },
-  methods: {
-      appChangeButton() {
-          this.message = 'Hello Vue ' + Math.floor(Math.random() * 10) + ' !'
-      }
-  }
+    data() {
+        return {
+            message: 'Hello Vue !',
+            dynamicId: Math.floor(Math.random() * 5),
+            dynamicClass: "dynamic"
+        }
+        /* return { helloAttributes: { message: 'Hello Vue !' } } */
+    },
+    methods: {
+        appChangeButton() {
+            this.message = '<span style="color:green">Hello Vue ' + Math.floor(Math.random() * 10) + ' !</span>'
+        }
+    }
 }
