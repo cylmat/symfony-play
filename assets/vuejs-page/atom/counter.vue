f1cce0d146f2ceb51a59f85ebc38661d9b14febf
cylmat <cyrilmatte.pro@gmail.com>
2024-01-22 16:01:22 +0000

    reactive

--- assets/vuejs-page/atom/counter.vue
+++ assets/vuejs-page/atom/counter.vue
@@ -1,16 +1,37 @@
+<!--  Single-File Components (SFCs). -->
+
+<script setup>
+    // declare reactive state
+    import { ref, reactive, onMounted } from 'vue'
+
+    const count = ref(0) // object value, used for primitives
+    
+    // JS Proxies reactive object
+    // proxy !== raw, reactive(raw) === proxy
+    const state = reactive({ count: 0 }) 
+
+    async function increment() {
+        count.value++
+        state.count++
+        await nextTick() // nextTick() global API
+        // Now the DOM is updated
+    }
+</script>
 
 <script>
     export default {
         data() {
             return {
-                count: 0
+                //count: 0
             }
         }
     }
 </script>
 
 <template>
-    <button @click="count++">Sfc Count is: {{ count }}</button>
+    <button @click="increment">
+        Sfc Count is: {{ count }}
+    </button>
 </template>
 
 <style scoped>
